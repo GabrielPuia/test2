080882ac350f1b6cd79485ceab536514080813f1
Gabriel Puia <gabrielpuia@gabriels-mbp.evointern>
2023-05-04 11:22:13 +0300

    new stuff

--- src/main/java/org/example/ClasaTest.java
+++ src/main/java/org/example/ClasaTest.java
@@ -3,13 +3,11 @@ package org.example;
 public class ClasaTest {
     private int amount;
 
-    private double no;
 
     private int id;
 
-    public ClasaTest(int amount, double no, int id) {
+    public ClasaTest(int amount, int id) {
         this.amount = amount;
-        this.no = no;
         this.id = id;
     }
 }
