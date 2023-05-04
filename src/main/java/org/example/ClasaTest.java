07c0d310293d2a374a3d5299cd2df5dd71338ab3
Gabriel Puia <gabrielpuia@gabriels-mbp.evointern>
2023-05-04 10:58:31 +0300

    Clasa added

---
+++ src/main/java/org/example/ClasaTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class ClasaTest {
+    private int amount;
+
+    private double no;
+
+    public ClasaTest(int amount, double no) {
+        this.amount = amount;
+        this.no = no;
+    }
+}
